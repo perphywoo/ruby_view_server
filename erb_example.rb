78c9aa970858a9c39045f127cb0caa5c3776640f
Perphy Woo <perphywoo@gmail.com>
2015-07-11 03:07:56 +0800

    homework section 1 KO

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,13 @@
 require 'erb'
 
-x = 42
+x = 4275
 template = ERB.new "The value of x is: <%= x %>"
+puts template.result(binding)
+
+y = 'wahahaha'
+template = ERB.new "The value of y is: <%= y %>"
+puts template.result(binding)
+
+z = ['AA','BB','CC','DD','EE']
+template = ERB.new "The value of z is: <%= z %>"
 puts template.result(binding)
